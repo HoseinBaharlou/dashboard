909ab0149c7cb4fdc87795f6b499372ff557af70
HoseinBaharlou <hossinbahar98@gmail.com>
2021-09-19 20:50:21 +0430

    change component vlist and navigation and change font

---
+++ src/components/Navbar/Vlist.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-list>
+        <v-list-item v-for='link in links' :key="link.title" :to="link.router" class="rounded-lg mx-3 indigo white--text">
+            <v-list-item-icon>
+                <v-icon>{{link.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+                <v-list-item-title>{{link.title}}</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-list>
+</template>
+<script>
+export default ({
+    name:'Vlist',
+    props:['links']
+})
+</script>
---
+++ src/components/dashboard/profile_statistics.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h3 class="indigo--text darken-4">آمار پروفایل</h3>
+        <div class="mt-16 d-flex">
+            <div class="d-flex">
+                <v-chip  class="deep-purple lighten-3 rounded-lg pa-5">
+                    <v-icon class="white--text" size="25">mdi-eye</v-icon>
+                </v-chip>
+                <span class="mr-4">
+                    <h4 class="grey--text"> تعداد نمایش پروفایل</h4>
+                    <p class="caption">120.000</p>
+                </span>
+            </div>
+            <div class="d-flex mr-5">
+                <v-chip  class="blue lighten-2 rounded-lg pa-5">
+                    <v-icon class="white--text" size="25">mdi-account</v-icon>
+                </v-chip>
+                <span class="mr-4">
+                    <h4 class="grey--text">دنبال کنندگان</h4>
+                    <p class="caption">120.000</p>
+                </span>
+            </div>
+            <div class="d-flex mr-5">
+                <v-chip  class="green lighten-2 rounded-lg pa-5">
+                    <v-icon class="white--text" size="25">mdi-account-plus</v-icon>
+                </v-chip>
+                <span class="mr-4">
+                    <h4 class="grey--text">دنبال شوندگان</h4>
+                    <p class="caption">120.000</p>
+                </span>
+            </div>
+            <div class="d-flex mr-5">
+                <v-chip  class="deep-orange darken-1 rounded-lg pa-5">
+                    <v-icon class="white--text" size="25">mdi-bookmark-minus</v-icon>
+                </v-chip>
+                <span class="mr-4">
+                    <h4 class="grey--text">پیام ذخیره شده</h4>
+                    <p class="caption">120.000</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
